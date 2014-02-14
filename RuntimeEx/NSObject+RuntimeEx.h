c894949b8fec82c044ce2c107041031829d5b0d3
satyso <song4@163.com>
2014-02-14 15:13:15 +0800

    增加对运行函数的扩展，目前只支持void类型的函数。

---
+++ RuntimeEx/NSObject+RuntimeEx.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+RuntimeEx.h
+//  RuntimeEx
+//
+//  Created by satyso on 14-2-13.
+//  Copyright (c) 2014年 song4@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (RuntimeEx)
+
+@end
---
+++ RuntimeEx/NSObject+RuntimeEx.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+RuntimeEx.m
+//  RuntimeEx
+//
+//  Created by satyso on 14-2-13.
+//  Copyright (c) 2014年 song4@163.com. All rights reserved.
+//
+
+#import "NSObject+RuntimeEx.h"
+
+@implementation NSObject (RuntimeEx)
+
+@end
